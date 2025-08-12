e9fbe79bcb6a80c56dd3f9f3ef101af4e8d2dfa8
Marti <m4rti21@proton.me>
2025-08-12 11:48:12 +0200

    no green

--- lua/black-metal/palette/impaled-nazarene.lua
+++ lua/black-metal/palette/impaled-nazarene.lua
@@ -2,7 +2,7 @@
 --- colors taken from Tol Cormpt Norz Norz Norz..., 1993
 local M = {
 	alt = "#5f8787",
-	alt_bg = "#191A11",
+	alt_bg = "#191919",
 	bg = "#000000",
 	comment = "#505050",
 	constant = "#aaaaaa",
