6f997758e209adac1f0cdca13fbb1bc22e238ed3
Marti <m4rti21@proton.me>
2025-08-12 13:07:32 +0200

    cols

--- lua/black-metal/palette/impaled-nazarene.lua
+++ lua/black-metal/palette/impaled-nazarene.lua
@@ -2,24 +2,24 @@
 --- colors taken from Tol Cormpt Norz Norz Norz..., 1993
 local M = {
 	alt = "#5f8787",
-	alt_bg = "#2C2727",
+	alt_bg = "#1f1919",
 	bg = "#000000",
-	comment = "#565252",
-	constant = "#f2f1f1",
-	fg = "#bebdbd",
-	func = "#5f8787",
-	keyword = "#B29740",
+	comment = "#505050",
+	constant = "#aaaaaa",
+	fg = "#c1c1c1",
+	func = "#B29740",
+	keyword = "#974b46",
 	line = "#000000",
-	number = "#a17fc0",
+	number = "#aaaaaa",
 	operator = "#9b99a3",
 	property = "#c1c1c1",
-	string = "#688A67", -- first accent
-	type = "#974b46", -- second accent
+	string = "#8BAE68", -- first accent
+	type = "#B29740", -- second accent
 	visual = "#333333",
 	diag_red = "#974b46",
 	diag_blue = "#5f8787",
 	diag_yellow = "#B29740",
-	diag_green = "#6e4c4c",
+	diag_green = "#8BAE68",
 }
 
 ---@type black-metal.Theme.Terminal
