a1bcc31d106ee699b19526619d0b0e88c50b69d5
Marti <m4rti21@proton.me>
2025-08-12 13:03:01 +0200

    wack

--- lua/black-metal/palette/impaled-nazarene.lua
+++ lua/black-metal/palette/impaled-nazarene.lua
@@ -2,23 +2,23 @@
 --- colors taken from Tol Cormpt Norz Norz Norz..., 1993
 local M = {
 	alt = "#5f8787",
-	alt_bg = "#191919",
+	alt_bg = "#2C2727",
 	bg = "#000000",
-	comment = "#505050",
-	constant = "#aaaaaa",
-	fg = "#c1c1c1",
-	func = "#888888",
-	keyword = "#999999",
+	comment = "#565252",
+	constant = "#f2f1f1",
+	fg = "#bebdbd",
+	func = "#5f8787",
+	keyword = "#B29740",
 	line = "#000000",
-	number = "#aaaaaa",
+	number = "#a17fc0",
 	operator = "#9b99a3",
 	property = "#c1c1c1",
-	string = "#974b46", -- first accent
-	type = "#B29740", -- second accent
+	string = "#688A67", -- first accent
+	type = "#974b46", -- second accent
 	visual = "#333333",
-	diag_red = "#5f8787",
-	diag_blue = "#999999",
-	diag_yellow = "#5f8787",
+	diag_red = "#974b46",
+	diag_blue = "#5f8787",
+	diag_yellow = "#B29740",
 	diag_green = "#6e4c4c",
 }
 
