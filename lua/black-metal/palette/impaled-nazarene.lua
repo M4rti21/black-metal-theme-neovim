0cce169deab4e9ae187d41015cf68b35e17c1200
Marti <m4rti21@proton.me>
2025-08-12 13:08:11 +0200

    a

--- lua/black-metal/palette/impaled-nazarene.lua
+++ lua/black-metal/palette/impaled-nazarene.lua
@@ -2,7 +2,7 @@
 --- colors taken from Tol Cormpt Norz Norz Norz..., 1993
 local M = {
 	alt = "#5f8787",
-	alt_bg = "#1f1919",
+	alt_bg = "#1c1919",
 	bg = "#000000",
 	comment = "#505050",
 	constant = "#aaaaaa",
